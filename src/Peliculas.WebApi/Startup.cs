9b94a1e54e718469f5beae5722a9ed4ef0a93614
esteban.quesada <esteban.quesada@E7470-EQUESADA>
2022-04-26 08:04:28 -0600

    Added Swagger

--- src/Peliculas.WebApi/Startup.cs
+++ src/Peliculas.WebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Peliculas.Data.Contexts;
 using Peliculas.Data.Repositories;
 
@@ -21,6 +22,11 @@ namespace Peliculas.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My EF Test API", Version = "v1" });
+            });
+
             services.AddControllers();
             services.AddDbContext<PeliculasContext>(
                 c => c.UseSqlServer(Configuration.GetConnectionString("EntityTest"), x => x.UseNetTopologySuite())
@@ -31,6 +37,12 @@ namespace Peliculas.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My EF Test API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
